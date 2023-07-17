8546effee9003be933bf08e68b0c027483b7682c
HoweZen <howezen@gmail.com>
2023-07-17 21:54:28 +0800

    Level 1 scene, player movement and tile spawn
    Player can now move left or right. Camera follow player. map will spawn and also despawn after player have cross it. all in scene level 1 for now

---
+++ MGD2 Working Product/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+/// <summary>
+/// Responsible for moving the player automatically and
+/// receiving input.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// A reference to the Rigidbody component
+    /// </summary>
+    private Rigidbody rb;
+    [Tooltip("How fast the ball moves left/right")]
+    public float dodgeSpeed = 5;
+    [Tooltip("How fast the ball moves forwards automatically")]
+    [Range(0, 50)]
+    public float rollSpeed = 5;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Get access to our Rigidbody component
+        rb = GetComponent<Rigidbody>();
+    }
+    /// <summary>
+    /// FixedUpdate is called at a fixed framerate and is a prime place
+    /// to put
+    /// Anything based on time.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Check if we're moving to the side
+        var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed;
+        rb.AddForce(horizontalSpeed, 0, rollSpeed);
+    }
+}
